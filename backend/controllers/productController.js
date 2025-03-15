d71e1024fdbbce63546d691032e4998492198094
Ofion01 <egorts.gentek@gmail.com>
2025-03-16 01:59:56 +0300

    сделал addProduct, добавление продукта в DB

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,6 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
+
 // add product, totalproduct list, remove product
 
 // function for add product
@@ -6,7 +9,7 @@ const addProduct = async (req, res) => {
     //get products details
     const {
       name,
-      discription,
+      description,
       price,
       category,
       subCategory,
@@ -15,23 +18,59 @@ const addProduct = async (req, res) => {
     } = req.body;
 
     //image
-    const image1 = req.files.image1[0];
-    const image2 = req.files.image2[0];
-    const image3 = req.files.image3[0];
-    const image4 = req.files.image4[0];
+    //если одно изображение не загружено = undefined(для этого изображения)
+    const image1 = req.files.image1 && req.files.image1[0]; //если image1 доступно(имеется),выпонляется команда
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image3 && req.files.image4[0];
+
+    //фильтр для undefined, чтобы не загружались пустые изображения(не имеющися на входе в respond) Благодяра этому можем загружать от 1 до 4 изображений, не обязательно 4
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
 
     console.log(
       name,
-      discription,
+      description,
       price,
       category,
       subCategory,
       sizes,
       bestseller
     );
-    console.log(image1, image2, image3, image4);
 
-    res.json({});
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price), //изначально получаем как String
+      subCategory,
+      bestseller: bestseller === "true" ? true : false, //изначально получаем как String
+      sizes: JSON.parse(sizes), //изначально получаем как String
+      image: imagesUrl, //получаем с cloud
+      date: Date.now(),
+    };
+
+    // отрабатывается при отправке на сервер запроса(при respond)
+    // console.log(imagesUrl); // узнали на каком url адресе хранится изображение
+    // console.log(images) // под какими атрибутами хранятся данные
+    console.log(productData);
+
+    const product = new productModel(productData);
+
+    //сохраняем продукт в DB
+    await product.save();
+
+    res.json({ success: true, message: "Product Added" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
